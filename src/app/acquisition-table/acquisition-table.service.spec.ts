3a9fa8a5c4f01c1486a9d0b79f02597b48b28307
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2018-05-07 16:51:35 -0700

    added acquisition generation service

---
+++ src/app/acquisition-table/acquisition-table.service.spec.ts
@@ -0,0 +1,59 @@
+import { async, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AcquisitionTableService } from './acquisition-table.service';
+import { MiniXService } from './minix.service';
+
+describe('Service: AcquisitionTableService', () => {
+  let service;
+  let miniXService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientModule ],
+      providers: [ AcquisitionTableService, MiniXService ]
+    });
+
+    service = TestBed.get(AcquisitionTableService);
+    miniXService = TestBed.get(MiniXService);
+  });
+
+  it('should shuffle an array of numbers', async(() => {
+    var sampleNumbers = service.generateSampleNumbers(100);
+    var result = service.generateRandomizedArray(sampleNumbers.slice(0));
+    expect(sampleNumbers).not.toBe(result);
+  }));
+
+  it('should generate acquisition table for MX373065', async(() => {
+    var acquisitionParameters = {
+      prefix: 'Connor',
+      miniXID: 373065,
+      methods: [
+        'posHILIC'
+      ],
+      methodBlank: {
+        enabled: true,
+        label: 'MtdBlank',
+        frequency: 10
+      },
+      qc: {
+        enabled: true,
+        label: 'BioRec',
+        frequency: 10
+      },
+      nist: {
+        enabled: false,
+        label: 'NIST',
+        frequency: 100
+      },
+      msms: {
+        enabled: true,
+        number: 4
+      }
+    };
+
+    miniXService.getMiniXExport(acquisitionParameters.miniXID, (error, result) => {
+      var result = service.generateAcquisitionTable(result, acquisitionParameters);
+    });
+  }));
+});
---
+++ src/app/acquisition-table/acquisition-table.service.ts
@@ -0,0 +1,114 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AcquisitionTableService {
+
+  constructor() { }
+
+  generateAcquisitionTable(miniXData, params) {
+    console.log(miniXData);
+    console.log(params);
+
+    var miniXID = miniXData.experiment.$.id;
+    var sampleData = [];
+
+    // Compile sample data
+    miniXData.experiment.classes[0].class.forEach(c => {
+      c.samples[0].sample.forEach(sample => {
+
+        sampleData.push({
+          id: sample.$.fileName,
+          minix: miniXID,
+          acquisition: {
+            instrument: miniXData.experiment.architecture[0].$.name
+          },
+          metadata: {
+            class: c.$.id,
+            species: c.$.species,
+            organ: c.$.organ
+          },
+          userdata: {
+            label: sample.$.label,
+            comment: sample.$.comment
+          }
+        });
+      });
+    });
+
+    // Generate acquisition table
+    var acquisitionTable = [];
+
+    var pad = n => ('' + n).padStart(3, '0')
+
+    var formatSampleName = (sample, i, j) => {
+      return miniXID +'_'+ params.prefix + pad(i) +'_'+ params.methods[0]
+        +'_'+ sample.userdata.label;
+    }
+
+    var formatQCName = (label, i, frequency) => {
+      return miniXID +'_'+ label + pad(i == 1 ? i : Math.ceil(i / frequency)) +'_'+
+        params.methods[0] +'_'+ (i == 1 ? 'pre' : 'post') + params.prefix + pad(i);
+    }
+
+    var fileNames = [];
+
+    this.generateRandomizedArray(sampleData).forEach((sample, i) => {
+      if (params.methodBlank.enabled && (i == 0 || (i + 1) % params.methodBlank.frequency == 0)) {
+        fileNames.push(formatQCName(params.methodBlank.label, i + 1, params.methodBlank.frequency))
+      }
+
+      if (params.qc.enabled && (i == 0 || (i + 1) % params.qc.frequency == 0)) {
+        fileNames.push(formatQCName(params.qc.label, i + 1, params.qc.frequency))
+      }
+
+      if (params.nist.enabled && (i == 0 (i + 1) % params.nist.frequency == 0)) {
+        fileNames.push(formatQCName(params.nist.label, i + 1, params.nist.frequency))
+      }
+
+      fileNames.push(formatSampleName(sample, i + 1));
+
+      if (params.methodBlank.enabled && i == sampleData.length - 1) {
+        fileNames.push(formatQCName(params.methodBlank.label, i + 1, params.methodBlank.frequency))
+      }
+
+      if (params.qc.enabled && i == sampleData.length - 1) {
+        fileNames.push(formatQCName(params.qc.label, i + 1, params.qc.frequency))
+      }
+    });
+
+    if (params.msms.enabled) {
+
+    }
+
+    console.log(fileNames)
+  }
+
+  /**
+   * Uses the Fisher-Yates (aka Knuth) Shuffle
+   * https://stackoverflow.com/a/2450976/406772
+   */
+  generateRandomizedArray(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
+  /**
+   * Generate an array from 1 to n
+   */
+  generateSampleNumbers(n: number) {
+    return Array.from({length: n}, (v, k) => k+1);
+  }
+}
