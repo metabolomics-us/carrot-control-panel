85777b96c7044c299cc1211416e0f9c0df549c9d
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2018-05-07 13:36:13 -0700

    updated acquisition table gui

--- src/app/acquisition-table/acquisition-table.component.ts
+++ src/app/acquisition-table/acquisition-table.component.ts
@@ -9,13 +9,43 @@ import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } 
 export class AcquisitionTableComponent implements OnInit {
 
   acquistionTableForm: FormGroup;
+  minixFileLabels = ['posCSH', 'negCSH', 'posHILIC', 'negHILIC'];
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
-    this.acquistionTableForm = new FormGroup({
-      minix: new FormControl('', [
+    this.acquistionTableForm = this.formBuilder.group({
+      studyLabel: new FormControl(''),
+
+      minix1: new FormControl('', [
         Validators.required,
         Validators.pattern("\d+")
-      ])
+      ]),
+      minix1_label: new FormControl(this.minixFileLabels),
+
+      minix2: new FormControl('', [
+        Validators.required,
+        Validators.pattern("\d+")
+      ]),
+      minix2_label: new FormControl(this.minixFileLabels),
+
+      blankEnabled: new FormControl(false),
+      blankLabel: new FormControl('Blank'),
+      blankFrequency: new FormControl(10),
+
+      methodBlankEnabled: new FormControl(false),
+      methodBlankLabel: new FormControl('MethBlank'),
+      methodBlankFrequency: new FormControl(10),
+
+      qcEnabled: new FormControl(false),
+      qcLabel: new FormControl('BioRec'),
+      qcFrequency: new FormControl(10),
+
+      nistEnabled: new FormControl(false),
+      nistLabel: new FormControl('NIST'),
+      nistFrequency: new FormControl(100),
+
+      msmsNumber: new FormControl(8)
     });
   }
 }
