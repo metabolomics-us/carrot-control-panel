6e484c06ccb240d95c1c4018489be195b0c55969
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2018-05-09 15:58:05 -0700

    finished ms/ms selection

--- src/app/acquisition-table/atf-msms.component.ts
+++ src/app/acquisition-table/atf-msms.component.ts
@@ -1,23 +1,59 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
+import { ATFComponent } from './atf.component';
+
+import * as cloneDeep from 'lodash/cloneDeep';
+
+
 @Component({
   selector: 'app-atf-msms',
   templateUrl: './atf-msms.component.html',
   styleUrls: []
 })
-export class ATFMSMSComponent implements OnInit {
+export class ATFMSMSComponent extends ATFComponent implements OnInit {
 
-  @Input()
-  data;
-
-  form: FormGroup;
-  method: string;
-
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder) {
+    super();
+  }
 
   ngOnInit() {
-    this.method = this.data.ionizations[0] + this.data.platform
-    this.form = this.formBuilder.group({});
+    var formFields = {};
+
+    this.data.sampleData.forEach((sample, i) => {
+      if (sample.userdata) {
+        formFields[i] = false
+      }
+    })
+
+    this.form = this.formBuilder.group(formFields);
+  }
+
+  countMSMS() {
+    var count = 0;
+
+    for (let sample in this.form.value) {
+      if (this.form.value[sample])
+        count++;
+    }
+
+    return count;
+  }
+
+  nextStep() {
+    // Add MS/MS samples
+    this.data.msmsData = [];
+
+    for (let i in this.form.value) {
+      if (this.form.value[i]) {
+        var sample = cloneDeep(this.data.sampleData[i]);
+        sample.filename = this.data.prefix +'_MSMS_MX'+ sample.filename.split('_MX')[1];
+
+        this.data.msmsData.push(sample);
+      }
+    }
+
+    window.scroll(0, 0);
+    this.data.step += 1;
   }
 }
