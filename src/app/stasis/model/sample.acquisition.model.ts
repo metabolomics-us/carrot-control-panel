6fb87c14c1583115615c2965a57233f61c31ece7
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2018-05-21 16:43:17 -0700

    remove name from sample acquisition model

--- src/app/stasis/model/sample.acquisition.model.ts
+++ src/app/stasis/model/sample.acquisition.model.ts
@@ -1,12 +1,10 @@
 export class Acquisition {
   instrument: string;
-  name: string;
   ionisation: string;
   method: string;
 
-  constructor(instrument: string, name: string, ionisation: string, method: string) {
+  constructor(instrument: string, ionisation: string, method: string) {
     this.instrument = instrument;
-    this.name = name;
     this.ionisation = ionisation;
     this.method = method;
   }
