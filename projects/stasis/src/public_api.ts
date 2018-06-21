76ddcc97a89bac9b88c3824bcd7efbcc6ae55514
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2018-06-21 14:37:16 -0700

    added all statis model classes to public_api

--- projects/stasis/src/public_api.ts
+++ projects/stasis/src/public_api.ts
@@ -2,5 +2,22 @@
  * Public API Surface of stasis
  */
 
+export * from './lib/model/sample.model';
+export * from './lib/model/sample.acquisition.model';
+export * from './lib/model/sample.processing.model';
+export * from './lib/model/sample.metadata.model';
+export * from './lib/model/sample.userdata.model';
+
+export * from './lib/model/tracking.model';
+export * from './lib/model/tracking.status.model';
+
+export * from './lib/model/result.model';
+export * from './lib/model/result.injection.model';
+export * from './lib/model/result.correction.model';
+export * from './lib/model/result.correction.point.model';
+export * from './lib/model/result.result.model';
+export * from './lib/model/result.target.model';
+export * from './lib/model/result.annotation.model';
+
 export * from './lib/stasis.service';
 export * from './lib/stasis.module';
