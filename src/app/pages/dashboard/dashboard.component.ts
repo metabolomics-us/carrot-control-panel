6fb50ce23142a21bce9f154f6c5d21b5936e7779
Diego Pedrosa <linuxmant@gmail.com>
2018-12-17 16:13:14 -0800

    fixed experiment paging

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-import { StasisService } from 'stasis';
-import { IExperimentParams } from 'stasis';
+import { StasisService, MessageService, TrackingData } from 'stasis';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { ExperimentPage } from 'dist/stasis/lib/model/experiment.page.model';
+import { ExperimentItem } from 'dist/stasis/lib/model/experiment.item.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,19 +13,45 @@ import { NgxSpinnerService } from 'ngx-spinner';
 export class DashboardComponent implements OnInit {
   @Input() sample: string = '';
   experiment: string = '';
+  @Input() pageSize: number = 25;
 
   resultData: any;
-  statusData: any[];
-  statusObject: Object;
+  statusData: Array<ExperimentItem>;
+  statusObject: any;
   selectedSample: string = undefined;
+  trackingData: TrackingData = undefined;
 
-  constructor(private stasisService: StasisService, private spinner: NgxSpinnerService) { }
+  constructor(private stasisService: StasisService, private spinner: NgxSpinnerService, private messageService: MessageService) { }
 
-  getExperimentData() {
+  getExperimentData(experiment: string = this.experiment, pageSize: number = this.pageSize, nextPage: string, clear = true) {
+
     this.spinner.show();
-    this.sample = undefined;
+    this.sample;
+    this.resultData;
+    this.statusData;
+
+    let response: ExperimentPage;
+
+    if(clear)
+      this.statusData = undefined;
+
+    this.stasisService.getExperiment(experiment, pageSize, nextPage).subscribe(
+      data => {
+        response = data
+      },
+      error => console.log("Error: " + error.message),
+      () => {
+        this.appendItems(response.items);
+        nextPage = response.last_item ? response.last_item.id : null;
+        console.log(`next: ${nextPage}`)
+        if(nextPage != null) {
+          this.getExperimentData(experiment, pageSize, nextPage, false)
+        }
+    });
+
     this.resultData = undefined;
-    this.stasisService.getExperiment({experiment: this.experiment, page: 25, lastSample: this.sample}).subscribe(data => { this.statusData = data['items']; this.resultData = undefined; }, () => {}, () => { this.hideSpinner() });
+    this.hideSpinner();
+    return response
   }
 
   getSampleData() {
@@ -32,10 +59,10 @@ export class DashboardComponent implements OnInit {
     this.experiment = undefined;
     this.resultData = undefined;
     this.stasisService.getResults(this.sample).subscribe(data => { this.resultData = data; }, () => { this.hideSpinner() }, () => { this.hideSpinner() });
-    this.stasisService.getTracking(this.sample).subscribe(data => { this.statusData = [data]; }, () => { this.hideSpinner() });
+    this.stasisService.getTracking(this.sample).subscribe(data => { this.trackingData = data; }, () => { this.hideSpinner() });
   }
 
-  ngOnInit() { 
+  ngOnInit() {
     this.resultData = undefined;
     this.stasisService.getStatuses().subscribe(data => { this.statusObject = data }, () => {}, () => {
       delete this.statusObject['processing'];
@@ -44,5 +71,13 @@ export class DashboardComponent implements OnInit {
 
   hideSpinner() {
     this.spinner.hide();
+  }
+
+  appendItems(items: ExperimentItem[]) {
+    if(this.statusData != null) {
+      this.statusData = this.statusData.concat(items);
+    } else {
+      this.statusData = items;
+    }
   }
 }
