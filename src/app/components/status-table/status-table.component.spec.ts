4467a0ac6d0783b8ffa78b7b00a05e9e1059de89
Daniel Rundle <derundle@ucdavis.edu>
2020-01-03 14:47:00 -0800

    Fixed error in status table component spec

--- src/app/components/status-table/status-table.component.spec.ts
+++ src/app/components/status-table/status-table.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 // import { fakeAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { StatusTableComponent } from './status-table.component';
 
@@ -11,9 +11,9 @@ describe('StatusTableComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      // imports: [
-      //   NgbModule
-      // ],
+      imports: [
+        NgbModule
+      ],
       declarations: [ 
         StatusTableComponent 
       ]
